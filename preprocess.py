8319134c1175760672e2ef460a59179ff8d411d6
developer <developer@tinynets.io>
2023-11-16 18:03:13 -0500

    added code to do basic analysis and set padding

--- preprocess.py
+++ preprocess.py
@@ -2,32 +2,51 @@
 This file is used to preprocess the data and clean it in preparation for training in a neural network
 """
 import os
+import numpy as np
 from torchtext.data.utils import get_tokenizer
 
 class Preprocess:
     def __init__(self, datadir):
         self.data_dir = datadir
+        self.raw = None
         self.data = self.load_data()
         self.preprocess_data()
         self.tokens = []
+        self.analysis = self.analyze_data()
+        
 
     def load_data(self):
         fnames = os.listdir(self.data_dir)
         lines = []
+        raw_lines = []
         for f in fnames:
-            with open(os.path.join(self.data_dir, f)) as f:
-                file_lines = f.readlines()
+            with open(os.path.join(self.data_dir, f)) as file:
+                file_lines = file.readlines()
+                raw_lines.extend(file_lines)
                 lines.extend(file_lines)
+        self.raw = raw_lines
         return lines
     
     def analyze_data(self):
+        analysis = {}
+        
+        clean_data = [line.strip().lower() for line in self.data if line.strip() != '']
+        print(clean_data)
+        line_lengths = [len(line) for line in clean_data]
 
-
-        return
+        analysis["avg_len"] = np.mean(line_lengths)
+        return analysis
     
     def preprocess_data(self):
         lines = [line.strip().lower() for line in self.data if line.strip()]
         self.data = lines
+
+    # defining here because one may want to run analysis and decide after viewing the data
+    def set_padding_size(self, size):
+        self.padding_size = size
+
+
+    
 
 
         
